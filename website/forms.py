4bf39a4a631e6fdf103d7f1db2ca4f80d9a6c3f7
Liliana Bassa <lilianabassa0510@gmail.com>
2024-06-19 10:56:40 +0200

    Create registration form

---
+++ website/forms.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(label="", widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Adres email'}))
+    first_name = forms.CharField(label="", max_length=100, widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Imię'}))
+    last_name = forms.CharField(label="", max_length=100, widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nazwisko'}))
+
+    class Meta():
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'Nazwa użytkownika'
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Wymagana. 150 znaków lub mniej. Tylko litery, cyfry i @/./+/-/_ .</small></span>'
+
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Hasło'
+        self.fields['password1'].label = ''
+        self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Twoje hasło nie może być podobne do Twoich danych osobowych.</li><li>Twoje hasło musi zawierać co najmniej 8 znaków.</li><li>Twoje hasło nie może być często używanym hasłem.</li><li>Twoje hasło nie może zawierać wyłącznie cyfr</li></ul>'
+
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Potwierdź hasło'
+        self.fields['password2'].label = ''
+        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Wpisz to samo hasło w celu weryfikacji.</small></span>'
