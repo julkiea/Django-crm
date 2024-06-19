d645ce952ceef51c610ff8efc025e5434932e874
Liliana Bassa <lilianabassa0510@gmail.com>
2024-06-19 13:42:24 +0200

    Update home view

--- website/views.py
+++ website/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all()
+
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,9 +23,9 @@ def home(request):
             messages.error(request, "Wystąpił błąd podczas logowania, spróbuj ponownie!")
             return redirect('home')
 
-    # If not logging in
+    # If not logging in, if logged in 
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 
